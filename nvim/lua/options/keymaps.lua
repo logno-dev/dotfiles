6038c4c4f73d34397af2183b0568112ff28d4372
Logan Bunch <loganbunch@gmail.com>
2023-12-28 23:08:04 -0800

    Thu Dec 28 11:08:04 PM PST 2023

--- nvim/lua/options/keymaps.lua
+++ nvim/lua/options/keymaps.lua
@@ -126,6 +126,8 @@ vim.keymap.set('n', '<leader>ww', ':wall<cr>', { desc = 'Save All' })
 vim.keymap.set('n', '<leader>wq', ':wq<cr>', { desc = 'Save and Quit' })
 vim.keymap.set('n', '<leader>qw', ':q<cr>', { desc = 'Quit buffer' })
 vim.keymap.set('n', '<leader>qqq', ':q!<cr>', { desc = 'Quit without saving' })
+vim.keymap.set('n', '<leader>ws', ':w !git add . && git commit -m "$(date)" && git push',
+  { desc = 'Save, commit, and push file to remote repo' })
 
 -- [[ Terminal ]]
 vim.keymap.set('n', '<leader>/', ':ToggleTerm<cr>', { desc = 'Toggle Terminal' })
