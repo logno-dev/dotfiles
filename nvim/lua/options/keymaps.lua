17a7f3082e83c987d8aa60dae0f37ce65710014f
Logan Bunch <loganbunch@gmail.com>
2023-12-29 13:35:15 -0800

    Fri Dec 29 01:35:15 PM PST 2023

--- nvim/lua/options/keymaps.lua
+++ nvim/lua/options/keymaps.lua
@@ -21,7 +21,7 @@ local function find_git_root()
   end
   return git_root
 end
-
+-- test line
 vim.keymap.set('n', '<leader>f', function()
   -- You can pass additional configuration to telescope to change theme, layout, etc.
   require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
