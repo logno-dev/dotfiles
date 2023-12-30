2ac4d0921b0b4f2c4c93326b25da1603e03a4ecd
Logan Bunch <loganbunch@gmail.com>
2023-12-30 19:02:43 +0000

    Sat Dec 30 07:02:43 PM UTC 2023

--- setup.sh
+++ setup.sh
@@ -14,19 +14,19 @@ yay -S rustup
 
 rustup default stable
 
-yay -S blueberry blueman bluez brightnessctl microsoft-edge-stable-bin btop calc eza fd ffmpeg lemurs gimp github-cli go grim hyprshot hyprland-git kitty kitty-shell-integration lazygit mako neovim nodejs npm pnpm polkit polkit-kde polkit-qt5 ripgrep swayidle thunar waylock wl-gammarelay-rs wofi workstyle-git wpaperd xclip xdg-desktop-portal-hyprland-git zsh waybar-hyprland-git gsettings-qt nwg-look-bin trash-cli lolcat ttf-proggy-vector
+yay -S blueberry blueman bluez brightnessctl microsoft-edge-stable-bin btop calc eza fd ffmpeg lemurs gimp github-cli go grim hyprshot hyprland-git kitty kitty-shell-integration lazygit mako neovim nodejs npm pnpm polkit polkit-kde polkit-qt5 ripgrep swayidle thunar waylock wl-gammarelay-rs wofi workstyle-git wpaperd xclip xdg-desktop-portal-hyprland-git zsh waybar-hyprland-git gsettings-qt nwg-look-bin trash-cli lolcat ttf-proggy-vector tmux ttf-nerd-fonts-symbols ttf-nerd-fonts-symbols-mono
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/plugins/zsh-autosuggestions
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/plugins/zsh-syntax-highlighting
 
-
-sudo systemctl enable bluetooth.service && sudo systemctl start bluetooth.service
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 cd ~
 rm .zshrc
 ln -s ~/.config/.zshrc ~/.zshrc
 
-echo "Setup complete, please reboot"
+sudo systemctl enable bluetooth.service && sudo systemctl start bluetooth.service
+
+echo "Setup complete. Lemurs will need manual configuration, please see documentation. Please reboot"
