ada9e898a356935ce6e8ab37092c3218319ae150
Logan Bunch <loganbunch@gmail.com>
2024-02-09 21:15:00 -0800

    Fri Feb  9 09:15:00 PM PST 2024

--- setup.sh
+++ setup.sh
@@ -14,7 +14,7 @@ yay -S rustup
 
 rustup default stable
 
-yay -S blueberry blueman bluez brightnessctl microsoft-edge-stable-bin btop calc eza fd ffmpeg lemurs gimp github-cli go grim hyprshot hyprland-git kitty kitty-shell-integration lazygit mako neovim nodejs npm pnpm polkit polkit-kde polkit-qt5 ripgrep swayidle thunar waylock wl-gammarelay-rs wofi workstyle-git wpaperd xclip xdg-desktop-portal-hyprland-git zsh waybar-hyprland-git gsettings-qt nwg-look-bin trash-cli lolcat ttf-proggy-vector tmux ttf-nerd-fonts-symbols ttf-nerd-fonts-symbols-mono
+yay -S brightnessctl btop calc lcd fd fzf ffmpeg lemurs gimp github-cli go grim hyprshot hyprland-git kitty kitty-shell-integration lazygit mako neovim nodejs npm pnpm polkit polkit-kde polkit-qt5 ripgrep swayidle thunar gtklock wl-gammarelay-rs wofi workstyle-git wpaperd xclip xdg-desktop-portal-hyprland-git zsh waybar-hyprland-git gsettings-qt nwg-look-bin trash-cli lolcat ttf-proggy-vector tmux ttf-nerd-fonts-symbols ttf-nerd-fonts-symbols-mono brave-bin
 
 
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/plugins/zsh-autosuggestions
