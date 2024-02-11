aa51f76058e16c63239f256d86a3f6a566449343
Logan Bunch <loganbunch@gmail.com>
2024-02-11 11:46:42 -0800

    Sun Feb 11 11:46:42 AM PST 2024

--- setup.sh
+++ setup.sh
@@ -14,14 +14,14 @@ yay -S rustup
 
 rustup default stable
 
-yay -S brightnessctl btop calc lcd fd fzf ffmpeg lemurs gimp github-cli go grim hyprshot hyprland-git kitty kitty-shell-integration lazygit mako neovim nodejs npm pnpm polkit polkit-kde polkit-qt5 ripgrep swayidle thunar gtklock wl-gammarelay-rs wofi workstyle-git wpaperd xclip xdg-desktop-portal-hyprland-git zsh waybar-hyprland-git gsettings-qt nwg-look-bin trash-cli lolcat ttf-proggy-vector tmux ttf-nerd-fonts-symbols ttf-nerd-fonts-symbols-mono brave-bin
+yay -S bluez blueberry-waytland brightnessctl btop calc lcd fd fzf ffmpeg lemurs gimp github-cli go grim hyprshot hyprland-git kitty kitty-shell-integration lazygit mako neovim nodejs npm pnpm polkit polkit-kde polkit-qt5 ripgrep swayidle thunar gtklock wl-gammarelay-rs wofi workstyle-git wpaperd xclip xdg-desktop-portal-hyprland-git zsh waybar-hyprland-git gsettings-qt nwg-look-bin trash-cli lolcat ttf-proggy-vector tmux ttf-nerd-fonts-symbols ttf-nerd-fonts-symbols-mono brave-bin
 
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/plugins/zsh-autosuggestions
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/plugins/zsh-syntax-highlighting
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 cd ~
 rm .zshrc
