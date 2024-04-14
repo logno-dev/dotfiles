1668840317eb8e1420e11833d185bdc5896d244e
Logan Bunch <loganbunch@gmail.com>
2024-04-13 23:27:39 -0700

    Sat Apr 13 11:27:39 PM PDT 2024

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 sudo pacman -S archlinux-keyring
 
-sudo pacman -S --needed git base-devel
+sudo pacman -S --needed base-devel
 cd ~
 git clone https://aur.archlinux.org/yay-bin.git
 cd yay-bin
@@ -14,7 +14,7 @@ yay -S rustup
 
 rustup default stable
 
-yay -S bluez blueberry-waytland brightnessctl btop calc lcd fd fzf ffmpeg lemurs gimp github-cli go grim hyprshot hyprland-git kitty kitty-shell-integration lazygit mako neovim nodejs npm pnpm polkit polkit-kde polkit-qt5 ripgrep swayidle thunar gtklock wl-gammarelay-rs wofi workstyle-git wpaperd xclip xdg-desktop-portal-hyprland-git zsh waybar-hyprland-git gsettings-qt nwg-look-bin trash-cli lolcat ttf-proggy-vector tmux ttf-nerd-fonts-symbols ttf-nerd-fonts-symbols-mono brave-bin
+yay -S bluez blueberry-wayland brightnessctl btop calc lcd fd fzf ffmpeg gimp github-cli go grim hyprshot hyprland kitty kitty-shell-integration lazygit mako nodejs npm pnpm yarn polkit polkit-kde polkit-qt5 ripgrep hypridle hyprlock thunar wl-gammarelay-rs wpaperd xclip xdg-desktop-portal-hyprland-git zsh waybar-hyprland-git gsettings-qt nwg-look-bin trash-cli lolcat ttf-proggy-vector tmux ttf-nerd-fonts-symbols ttf-nerd-fonts-symbols-mono brave-bin tofi
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
